67f92d1db2ef8af0b25cd18a9d45c39b59020205
Morgan Moyer <mcmoyer11@gmail.com>
2020-04-15 14:14:03 -0400

    added DashedSentence

--- data_includes/cb_main.js
+++ data_includes/cb_main.js
@@ -1,8 +1,16 @@
 PennController.ResetPrefix(null); // Initiates PennController
-
+PennController.DebugOff()
 // CHEMLA & BOTT TRIALS
 
-Sequence("intro","consent", "demo","instructions", "trainT", "trainF", "train_thinkT", "train_thinkF", "end_train", rshuffle("trial"), "send", "final")
+Sequence("intro","consent", "demo","instructions", "Train", "end_train", rshuffle("test"), "send", "end")
+// Sequence("demo","instructions", "Train", "send")
+
+
+// This only works when I don't embed a DashedSentence in a newController()
+var defaults = [
+    "DashedSentence", {mode: "speeded acceptability", wordTime: 250}
+    ];
+
 
 newTrial("intro",
     defaultText
@@ -22,6 +30,7 @@ newTrial("intro",
         .print()
         .wait()
 )
+.setOption("hideProgressBar", true); // Do not show the progress bar on first screen
 
 newTrial("consent",
     newHtml("consent.html")
@@ -52,6 +61,7 @@ newTrial("consent",
         .set( getTextInput("ID") )
 )
 .log( "ID" , getVar("ID") )
+.setOption("hideProgressBar", true); // Do not show the progress bar on first screen
 
 newTrial("demo",
     defaultText
@@ -92,6 +102,7 @@ newTrial("demo",
 )
 .log( "NativeLang" , getVar("NativeLang") )
 .log( "OtherLangs" , getVar("OtherLangs") )
+.setOption("hideProgressBar", true); // Do not show the progress bar on first screen
 
 
 newTrial("instructions",
@@ -101,163 +112,80 @@ newTrial("instructions",
     ,
     newText("<p>In this experiment, we will ask you to decide whether you agree or disagree with a statement.</p>")
     ,
-    newButton("Let's practice!")
+    newText("<p>The statement will be revealed automatically, one word at a time.</p>")
+    ,
+    newText("<p>As each new word appears, the previous word will disappear.</p>")
+    ,
+    newText("<p>If you <strong>Agree</strong> with the sentence, press <strong>F</strong>. If you <strong>Disagree</strong>, then press <strong>J</strong><p>")
+    ,
+    newText("<p>Let's <strong>practice</strong> a few times before proceeding to the experiment.<p>")
+    ,
+    newButton("Click here when you're ready to Continue.")
         .center()
         .print()
         .wait()
 )
 
-
-newTrial( "trainT",
-        newTimer(500)
-            .start()
-            .wait()
-        ,
-        newText("<p><strong>PRACTICE.</strong></p>")
-            .center()
-            .print()
-        ,
-        newText( "<p>Houses can be made of wood.</p>" )
-            .center()
-            .print()
-        ,
-        newText("<p> If you <strong>Agree</strong> with the sentence, press <strong>F</strong>. If you <strong>Disagree</strong>, then press <strong>J</strong><p>")
-            .center()
-            .print()
-        ,
-        newText("<p>Since this statement is <strong>correct</strong>, you should press <strong>J</strong> on the keyboard to agree.<p>")
-            .center()
-            .print()
-        ,
-        newSelector()
-            .add( newText("Agree"), newText("Disagree"))
-            .center()
-            .keys("F", "J")
-            .log()
-            .wait()
-        ,
-        newTimer(500)
-            .start()
-            .wait()
+Template( "train_table.csv", row =>
+    [
+        "Train",
+        "DashedSentence", {s: row.Sentence},
+        "PennController", newTrial("question",    
+            newText("<p> Press <strong>F</strong> to <strong>Agree</strong> or <strong>J</strong> to <strong>Disagree</strong><p>")
+                .center()
+                .print()
+            ,
+            newText(row.Feedback)
+                .center()
+                .print()
+                .log( "Feedback" , row.Feedback )
+            ,
+            newSelector()
+                .add( newText("Agree"), newText("Disagree"))
+                .keys("F", "J")
+                .log("first")
+                .wait()
+            )
+            .log( "ID" , getVar("ID") )
+            .log( "Type" , row.Type )
+            .log( "Sentence" , row.Sentence )
+            .log( "Feedback" , row.Feedback )
+    ]
 )
 
-newTrial( "trainF",
-        newTimer(500)
-            .start()
-            .wait()
-        ,
-        newText("<p><strong>PRACTICE.</strong></p>")
-            .center()
-            .print()
-        ,
-        newText( "<p>Airplanes fly under water.</p>" )
-            .center()
-            .print()
-        ,
-        newText("<p> If you <strong>Agree</strong> with the sentence, press <strong>F</strong>. If you <strong>Disagree</strong>, then press <strong>J</strong><p>")
-            .center()
-            .print()
-        ,
-        newText("<p>Since this statement is <strong>incorrect</strong>, you should press <strong>J</strong> on the keyboard to disagree.<p>")
-            .center()
-            .print()
-        ,
-        newSelector()
-            .add( newText("Agree"), newText("Disagree"))
-            .center()
-            .keys("F", "J")
-            .log()
-            .wait()
-        ,
-        newTimer(500)
-            .start()
-            .wait()
-)
-
-newTrial( "train_thinkT",
-        newTimer(500)
-            .start()
-            .wait()
-        ,
-        newText("<p><strong>PRACTICE.</strong></p>")
-            .center()
-            .print()
-        ,
-        newText( "<p>Historians think that George Washington was a US president.</p>" )
-            .center()
-            .print()
-        ,
-        newText("<p> If you <strong>Agree</strong> with the sentence, press <strong>F</strong>. If you <strong>Disagree</strong>, then press <strong>J</strong><p>")
-            .center()
-            .print()
-        ,
-        newText("<p>Since this statement is <strong>correct</strong>, you should press <strong>F</strong> on the keyboard to agree.<p>")
-            .center()
-            .print()
-        ,
-        newSelector()
-            .add( newText("Agree"), newText("Disagree"))
-            .center()
-            .keys("F", "J")
-            .log()
-            .wait()
-        ,
-        newTimer(500)
-            .start()
-            .wait()
-)
-
-newTrial( "train_thinkF",
-        newTimer(500)
-            .start()
-            .wait()
-        ,
-        newText("<p><strong>PRACTICE.</strong></p>")
-            .center()
-            .print()
-        ,
-        newText( "<p>Geographers think that Spain is in Africa.</p>" )
-            .center()
-            .print()
-        ,
-        newText("<p> If you <strong>Agree</strong> with the sentence, press <strong>F</strong>. If you <strong>Disagree</strong>, then press <strong>J</strong><p>")
-            .center()
-            .print()
-        ,
-        newText("<p>Since this statement is <strong>incorrect</strong>, you should press <strong>J</strong> on the keyboard to disagree.<p>")
-            .center()
-            .print()
-        ,
-        newSelector()
-            .add( newText("Agree"), newText("Disagree"))
-            .center()
-            .keys("F", "J")
-            .log()
-            .wait()
-        ,
-        newTimer(500)
-            .start()
-            .wait()
-)
-
-
-// What is in Header happens at the beginning of every single trial
-Header(
-    // We will use this global Var element later to store the participant's name
-    newVar("ID")
-        .global()
-    ,
-    // Delay of 250ms before every trial
-    newTimer(250)
-        .start()
-        .wait()
-)
-.log( "ID" , getVar("ID") )
-// This log command adds a column reporting the participant's name to every line saved to the results
-
+// Template( "train_table.csv", row =>
+//     newTrial("Train",
+//         newText("<p><strong>PRACTICE</strong></p>")
+//             .center()
+//             .print()
+//         ,
+//         newController("DashedSentence", {s: row.Sentence})
+//             .center()
+//             .print()
+//             .log()
+//             .wait()
+//         ,
+//         newText("<p> Press <strong>F</strong> to <strong>Agree</strong> or <strong>J</strong> to <strong>Disagree</strong><p>")
+//             .center()
+//             .print()
+//         ,
+//         newText(row.Feedback)
+//             .center()
+//             .print()
+//         ,
+//         newSelector()
+//             .add( newText("Agree"), newText("Disagree"))
+//             .keys("F", "J")
+//             .log("first")
+//             .wait()
+//     )
+//     .log( "ID" , getVar("ID") )
+//     .log( "Type" , row.Type )
+//     .log( "Sentence" , row.Sentence )
+//     .log( "Feedback" , row.Feedback )
+// )
 
 newTrial("end_train",
-    
     defaultText
         .center()
         .print()
@@ -313,6 +241,41 @@ Template( "cb_table.csv", row =>
     .log( "MismatchingCategory" , row.MismatchingCategory )
     .log( "Group"  , row.Group  )
 )
+
+
+
+// This works to get speeded acceptability
+Template( "cb_table.csv", row =>
+    ["test",
+        "DashedSentence", {s: `${row.Matrix} ${row.Token} are ${row[row.Predicate.replace("Category",row.WhichCategory+'Category')]}`},
+        "PennController", newTrial("question",    
+            newText("<p> Press <strong>F</strong> to <strong>Agree</strong> or <strong>J</strong> to <strong>Disagree</strong><p>")
+                .center()
+                .print()
+            ,
+            newSelector()
+                .add( newText("Agree"), newText("Disagree"))
+                .keys("F", "J")
+                .log("first")
+                .wait()
+        )
+        .log( "ID" , getVar("ID") )
+        .log( "SentNumber" , row.SentNumber )
+        .log( "WhichCategory" , row.WhichCategory )
+        .log( "Matrix" , row.Matrix )
+        .log( "Subject" , row.Subject )
+        .log( "Predicate" , row.Predicate )
+        .log( "SentType" , row.SentType )
+        .log( "Negation" , row.Negation )
+        .log( "CompTruth" , row.CompTruth )
+        .log( "SubStat" , row.SubStat )
+        .log( "Token" , row.Token )
+        .log( "MatchingCategory" , row.MatchingCategory )
+        .log( "MismatchingCategory" , row.MismatchingCategory )
+        .log( "Group"  , row.Group  )
+    ]
+)
+
 
 SendResults( "send" )
 
